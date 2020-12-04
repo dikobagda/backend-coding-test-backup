11ef83aa369fd9c6bf886a21cf26c053b49ea5a0
dikobagda <dikobagda@gmail.com>
2020-12-05 05:01:38 +0700

    fixing variable not defined in test api

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const request = require('supertest');
-
+const mocha = require('mocha');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
+const describe = mocha.describe;
+const before = mocha.before;
+const it = mocha.it;
 const app = require('../src/app')(db);
 const buildSchemas = require('../src/schemas');
 
